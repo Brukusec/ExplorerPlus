698b9cbe16360f0a63126a58851797f42911cc1c
Bruno <Bruno@DESKTOP-05Q4GBC>
2023-08-23 10:38:04 +0100

    Add project files.

---
+++ ExplorerPlus/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using ExplorerPlus.Properties;
+
+namespace ExplorerPlus
+{
+    partial class Form1
+    {
+        // Designer components
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.StatusBar statusBar;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem cmdMenuItem;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Initialize the form components.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+
+            // Initialize components
+            this.backButton = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.statusBar = new System.Windows.Forms.StatusBar();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmdMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+
+            // Configure form
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.backButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Explorer+";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            // Configure backButton
+            this.backButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(133)))), ((int)(((byte)(244)))));
+            this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backButton.ForeColor = System.Drawing.Color.White;
+            this.backButton.Location = new System.Drawing.Point(10, 10);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 30);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = false;
+            this.backButton.Click += new System.EventHandler(this.OnBackButtonClicked);
+
+            // Configure listView
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(10, 50);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(780, 390);
+            this.listView.SmallImageList = this.imageList;
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.Columns.Add("Name");
+            this.listView.Columns.Add("Type");
+            this.listView.DoubleClick += new System.EventHandler(this.OnFileDoubleClicked);
+
+            // Configure imageList
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+
+            // Configure statusBar
+            this.statusBar.Location = new System.Drawing.Point(0, 427);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.ShowPanels = true;
+            this.statusBar.Size = new System.Drawing.Size(800, 23);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "Current Path: C:\\";
+
+            // Configure pathTextBox
+            this.pathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathTextBox.Location = new System.Drawing.Point(95, 10);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(695, 25);
+            this.pathTextBox.TabIndex = 3;
+            this.pathTextBox.Text = "C:\\";
+            this.pathTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pathTextBox_KeyDown);
+
+            // Configure contextMenuStrip
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.cmdMenuItem
+            });
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(100, 26);
+
+            // Configure cmdMenuItem
+            this.cmdMenuItem.Name = "cmdMenuItem";
+            this.cmdMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.cmdMenuItem.Text = "CMD";
+            this.cmdMenuItem.Click += new System.EventHandler(this.CmdMenuItem_Click);
+
+            // Add context menu to listView
+            this.listView.ContextMenuStrip = this.contextMenuStrip;
+        }
+    }
+}
---
+++ ExplorerPlus/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExplorerPlus
+{
+    public partial class Form1 : Form
+    {
+        private string currentPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("This application only works with standalone executables.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            LoadDirectory(Application.StartupPath);
+            EnsureLogFileExists();
+        }
+
+        // Navigate to the parent directory
+        private void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (currentPath != null && Directory.GetParent(currentPath) != null)
+            {
+                string parentPath = Directory.GetParent(currentPath).FullName;
+                LoadDirectory(parentPath);
+                pathTextBox.Text = parentPath;
+            }
+        }
+
+        // Handle file or directory double-click
+        private void OnFileDoubleClicked(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                var selectedItem = listView.SelectedItems[0];
+                var path = selectedItem.Tag.ToString();
+
+                if (Directory.Exists(path))
+                {
+                    LoadDirectory(path);
+                    pathTextBox.Text = path;
+                }
+                else
+                {
+                    ExecuteFile(path);
+                }
+            }
+        }
+
+        // Load the specified directory into the ListView
+        private void LoadDirectory(string path)
+        {
+            listView.Items.Clear();
+            currentPath = path;
+            statusBar.Text = "Current Path: " + currentPath;
+            pathTextBox.Text = currentPath;
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                var item = new ListViewItem(new[] { Path.GetFileName(dir), "Folder" });
+                item.Tag = dir;
+                listView.Items.Add(item);
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var item = new ListViewItem(new[] { Path.GetFileName(file), "File" });
+                item.Tag = file;
+                listView.Items.Add(item);
+            }
+
+            listView.Columns[0].Width = -2;
+        }
+
+        // Handle directory path input
+        private void pathTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newPath = pathTextBox.Text;
+                if (Directory.Exists(newPath))
+                {
+                    LoadDirectory(newPath);
+                }
+                else
+                {
+                    MessageBox.Show("The specified path does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Ensure the log file exists
+        private void EnsureLogFileExists()
+        {
+            string logFileName = "timestamp_log.log";
+            string logFilePath = Path.Combine(Application.StartupPath, logFileName);
+
+            if (!File.Exists(logFilePath))
+            {
+                using (StreamWriter sw = File.CreateText(logFilePath))
+                {
+                    sw.WriteLine("Data\tHora\tAction\tFilename");
+                }
+            }
+        }
+
+        // Log actions to the log file
+        private void LogAction(string action, string filename)
+        {
+            string logFileName = "timestamp_log.log";
+            string logFilePath = Path.Combine(Application.StartupPath, logFileName);
+
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                sw.WriteLine($"{DateTime.Now:yyyy-MM-dd}\t{DateTime.Now:HH:mm:ss}\t{action}\t{filename}");
+            }
+        }
+
+        // Execute the specified file
+        private void ExecuteFile(string path)
+        {
+            var tempFolder = Path.Combine("C:\\", Path.GetRandomFileName());
+            Directory.CreateDirectory(tempFolder);
+
+            var tempFilePath = Path.Combine(tempFolder, Path.GetFileName(path));
+            File.Copy(path, tempFilePath);
+
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo(tempFilePath),
+                EnableRaisingEvents = true
+            };
+
+            process.Exited += (s, args) =>
+            {
+                LogAction("Closed", Path.GetFileName(tempFilePath));
+                try
+                {
+                    File.Delete(tempFilePath);
+                    Directory.Delete(tempFolder);
+                }
+                catch
+                {
+                    // Handle potential errors
+                }
+            };
+
+            LogAction("Open", Path.GetFileName(path));
+            process.Start();
+        }
+
+        // Handle the CMD context menu item click
+        private void CmdMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                var selectedItem = listView.SelectedItems[0];
+                var path = selectedItem.Tag.ToString();
+
+                if (!Directory.Exists(path))
+                {
+                    var tempFolder = Path.Combine("C:\\", Path.GetRandomFileName());
+                    Directory.CreateDirectory(tempFolder);
+
+                    var tempFilePath = Path.Combine(tempFolder, Path.GetFileName(path));
+                    File.Copy(path, tempFilePath);
+
+                    var cmdProcess = new System.Diagnostics.Process
+                    {
+                        StartInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe", $"/k cd {tempFolder} & {Path.GetFileName(tempFilePath)}"),
+                        EnableRaisingEvents = true
+                    };
+
+                    cmdProcess.Exited += (s, args) =>
+                    {
+                        LogAction("Cmd Closed", Path.GetFileName(path));
+                        try
+                        {
+                            File.Delete(tempFilePath);
+                            Directory.Delete(tempFolder);
+                        }
+                        catch
+                        {
+                            // Handle potential errors
+                        }
+                    };
+
+                    cmdProcess.Start();
+                    LogAction("Cmd Opened", Path.GetFileName(path));
+                }
+            }
+        }
+    }
+}
